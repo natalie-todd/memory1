57112ddfc81aedfab7b0976d1448a29d33a8e3e6
Natalie <natalie.todd15@gmail.com>
2018-08-05 20:17:58 -0600

    seeds are in there

--- seeds/somedata.js
+++ seeds/somedata.js
@@ -1,13 +1,15 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('resolution').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, dueDate:, resolution:},
-        {id: 2, dueDate:, resolution:},
-        {id: 3, dueDate:, resolution:}
+      return knex('resolution').insert([
+        {id: 1, dueDate: '1997-02-01', resolution: 'Go skiing'},
+        {id: 2, dueDate: '1997-05-01', resolution: 'Do stand-up'},
+        {id: 3, dueDate: '1997-09-01', resolution: 'Start knitting'}
       ]);
+    }).then(() => {
+      return knex.raw('ALTER SEQUENCE resolution_id_seq RESTART WITH 4;')
     });
 };
