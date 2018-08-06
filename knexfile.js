b122e67a7b071f20b4d1537c5c5720dd10efee78
Natalie <natalie.todd15@gmail.com>
2018-08-05 20:08:27 -0600

    seeds have been partially added

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client:     'pg',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/resolution',
+  },
+
+  production: {
+    client:     'pg',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/resolution',
+  },
+
+};
